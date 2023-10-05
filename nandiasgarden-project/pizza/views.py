e5af706a0201e1d11347e3ac8ae1ed846d5784f8
datttrian <datttrian@gmail.com>
2023-10-05 18:38:53 +0000

    Editing objects

--- nandiasgarden-project/pizza/views.py
+++ nandiasgarden-project/pizza/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import PizzaForm, MultiplePizzaForm
 from django.forms import formset_factory
+from .models import Pizza
 
 
 # Create your views here.
@@ -13,13 +14,15 @@ def order(request):
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST)  # , request.FILES
         if filled_form.is_valid():
-            filled_form.save()
+            created_pizza = filled_form.save()
+            created_pizza_pk = created_pizza.id
             note = f'Thanks for ordering! Your {filled_form.cleaned_data["size"]} {filled_form.cleaned_data["topping1"]} and {filled_form.cleaned_data["topping2"]} pizza is on its way!'
             new_form = PizzaForm()
             return render(
                 request,
                 'pizza/order.html',
                 {
+                    'created_pizza_pk': created_pizza_pk,
                     'pizzaform': new_form,
                     'note': note,
                     'multiple_form': multiple_form,
@@ -54,3 +57,16 @@ def pizzas(request):
         )
     else:
         return render(request, 'pizza/pizzas.html', {'formset': formset})
+
+
+def edit_order(request, pk):
+    pizza = Pizza.objects.get(pk=pk)
+    form = PizzaForm(instance=pizza)
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST, instance=pizza)
+        if filled_form.is_valid():
+            filled_form.save()
+            form = filled_form
+    return render(
+        request, 'pizza/edit_order.html', {'pizzaform': form, 'pizza': pizza}
+    )
