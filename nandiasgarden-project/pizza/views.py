e0ff263eb03d8685cd32e0f94faea31217fa0108
datttrian <datttrian@gmail.com>
2023-10-05 17:37:59 +0000

    Using submitted data

--- nandiasgarden-project/pizza/views.py
+++ nandiasgarden-project/pizza/views.py
@@ -8,5 +8,16 @@ def home(request):
 
 
 def order(request):
-    form = PizzaForm()
-    return render(request, 'pizza/order.html', {'pizzaform': form})
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = f'Thanks for ordering! Your {filled_form.cleaned_data["size"]} {filled_form.cleaned_data["topping1"]} and {filled_form.cleaned_data["topping2"]} pizza is on its way!'
+            new_form = PizzaForm()
+            return render(
+                request,
+                'pizza/order.html',
+                {'pizzaform': new_form, 'note': note},
+            )
+    else:
+        form = PizzaForm()
+        return render(request, 'pizza/order.html', {'pizzaform': form})
